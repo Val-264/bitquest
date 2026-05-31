cdd658643017ae24ae79b319d425ded02025ee2c
LuisBernardo <arenasluisben@gmail.com>
2026-05-30 22:15:40 -0600

    Implementacion del bucle de captura de teclas WASD y movimiento libre (falta barreras)

--- main.c
+++ main.c
@@ -16,12 +16,14 @@ int main () {
 
     //2. BUCLE PRINCIPAL DEL JUEGO (Game Loop)
     char tecla = ' ';
+    int pasos = 0;
     while(tecla != 'q' && tecla != 'Q'){
 
         //Llamamos a la funcion de C que calcula e imprime la ventana visible
         imprimir_mapa();
 
         printf("\nMonedas en este nivel: %d\n", monedas_totales);
+        printf("Pasos realizados: %d\n", pasos);
         printf("Posicion del jugador: (%d, %d)\n", jugador_x, jugador_y);
         printf("Controles: W (Arriba), A (Izquierda), S (Abajo), D (Derecha) | Q (Salir)\n", jugador_x, jugador_y);
 
@@ -31,9 +33,50 @@ int main () {
         //Guardamos las coordenadas a donde el jugador desea ir
         int proximo_x = jugador_x;
         int proximo_y = jugador_y;
+        int intento_movimiento = 0;
 
-        // TODO: Modificar proximo_x o proximo_y usando un switch(tecla) según W, A, S, D [cite: 74, 75]
-        // Ejemplo: si presiona 'w', proximo_x = jugador_x - 1; incrementar pasos;
+        //Evaluamos la tecla presionada
+        switch(tecla){
+            case 'w': case 'W':
+            proximo_x = jugador_x - 1;
+            intento_movimiento = 1;
+            break;
+
+            case 's': case 'S':
+            proximo_x = jugador_x + 1;
+            intento_movimiento = 1;
+            break;
+
+            case 'a': case 'A':
+            proximo_y = jugador_y - 1;
+            intento_movimiento = 1;
+            break;
+
+            case 'd': case 'D':
+            proximo_y = jugador_y + 1;
+            intento_movimiento = 1;
+            break;
+        }
+
+        if(intento_movimiento){
+            // TODO: Aquí llamaremos formalmente a validar_movimiento en NASM 
+            // De momento, dejamos un validador temporal en C para probar la cámara:
+
+            if(proximo_x >= 0 && proximo_x < FILAS && proximo_y >= 0 && proximo_y < COLUMNAS){
+                // 1. Borramos al jugador de su posición vieja en la matriz gigante (se vuelve espacio libre)
+                mapa_nivel1[jugador_x][jugador_y] = 0;
+                
+                // 2. Actualizamos las coordenadas globales a la nueva ubicación
+                jugador_x = proximo_x;
+                jugador_y = proximo_y;
+                
+                // 3. Colocamos el valor del jugador (1) en la nueva celda de la matriz
+                mapa_nivel1[jugador_x][jugador_y] = 1;
+                
+                // 4. Incrementamos el contador de pasos de la sesión [cite: 157, 194]
+                pasos++;
+            }
+        }
 
         // TODO: Validar el movimiento llamando a la función obligatoria de NASM: 
         // int movimiento_valido = validar_movimiento((char *)mapa_nivel1, COLUMNAS, proximo_x, proximo_y);
@@ -49,9 +92,9 @@ int main () {
         // - Si detecta salida (6): Terminar nivel actual, mostrar resumen y cargar Nivel 2.
     }
     // 3. PANTALLA FINAL / RESUMEN
-    // TODO: Calcular puntaje final usando la rutina obligatoria en NASM 'calcular_puntaje' [cite: 154, 163]
+    // TODO: Calcular puntaje final usando la rutina obligatoria en NASM 'calcular_puntaje' 
     // TODO: Mostrar estadísticas totales del juego
 
-    printf("\nGracias por jugar BitQuest. ¡Hasta la proxima!\n");
+    printf("\nGracias por jugar BitQuest\n");
     return 0;
 }
